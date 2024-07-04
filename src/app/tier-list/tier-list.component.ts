925a3b0d5bc955ad3752229660d854ee47d7e20c
OFFBLACKgroup <offblack.group@gmail.com>
2024-07-04 19:47:19 +0200

    initial tier list layout

--- src/app/tier-list/tier-list.component.ts
+++ src/app/tier-list/tier-list.component.ts
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './tier-list.component.css'
 })
 export class TierListComponent {
-
+  tiers = ['S', 'A', 'B', 'C', 'D', 'E', 'F']
+  backgroundColors = ['#FF3131', '#FF7518', '#FFBF00', '#32CD32', '#00FFFF', '#1F51FF', '#DA70D6']
 }
