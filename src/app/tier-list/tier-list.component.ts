b967aaf6cc794fc826decb2ff07df772409bb191
OFFBLACKgroup <offblack.group@gmail.com>
2024-07-04 18:12:38 +0200

    responsive tier-list-header

---
+++ src/app/tier-list/tier-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tier-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './tier-list.component.html',
+  styleUrl: './tier-list.component.css'
+})
+export class TierListComponent {
+
+}
