10d6234f0bdaf96b962b1d1769f8c2e38acf3cf8
OFFBLACKgroup <offblack.group@gmail.com>
2024-07-05 15:18:21 +0200

    tier list drag & drop

--- src/app/tier-list/tier-list.component.ts
+++ src/app/tier-list/tier-list.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
+import {CdkDrag, CdkDropList, CdkDragDrop, CdkDragEnd, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-tier-list',
   standalone: true,
-  imports: [],
+  imports: [CdkDrag, CdkDropList],
   templateUrl: './tier-list.component.html',
   styleUrl: './tier-list.component.css'
 })
 export class TierListComponent {
   tiers = ['S', 'A', 'B', 'C', 'D', 'E', 'F']
   backgroundColors = ['#FF3131', '#FF7518', '#FFBF00', '#32CD32', '#00FFFF', '#1F51FF', '#DA70D6']
+
+  drop(e: CdkDragDrop<string[]>) {
+    this.tierData.splice(e.currentIndex, 0, e.item.data)
+  }
+
+  nextUp = "../../assets/tokyo revengers.png"
+  tierData: string[] = []
+
+  sortPredicate(index: number, item: CdkDrag<number>) {
+    return index == 0;
+  }
+
+  test() {}
 }
