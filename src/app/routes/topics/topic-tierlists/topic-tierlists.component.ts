a2300ab649db18b5d4f1d6e80109f898996c790f
OFFBLACKgroup <offblack.group@gmail.com>
2024-07-08 17:12:07 +0200

    basic topic-tierlists view

--- src/app/routes/topics/topic-tierlists/topic-tierlists.component.ts
+++ src/app/routes/topics/topic-tierlists/topic-tierlists.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HeadlineComponent } from '../../../components/headline/headline.component';
+import { HttpService } from '../../../http-service.service';
 
 @Component({
   selector: 'app-topic-tierlists',
   standalone: true,
-  imports: [],
+  imports: [HeadlineComponent],
   templateUrl: './topic-tierlists.component.html',
   styleUrl: './topic-tierlists.component.css'
 })
 export class TopicTierlistsComponent {
+  constructor (private http: HttpService) {}
 
+  @Input() title = 'Tier lists'
+
+  points = 2
+
+  tierLists = [0,0,0,0,0,0,0,0,0]
 }
