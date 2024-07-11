616a95afedc999301371fe91243e6a338d0affbb
OFFBLACKgroup <offblack.group@gmail.com>
2024-07-11 10:43:26 +0200

    image preload + used for topic images

--- src/app/routes/main-menu/topics/topics.component.ts
+++ src/app/routes/main-menu/topics/topics.component.ts
@@ -1,10 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { HeadlineComponent } from '../../../components/headline/headline.component';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
-export function preloadImages() {
-  
+export function preloadImages(imageSources: string[], loadingData: {areImagesLoaded: boolean, numOfLoaded: number}) {
+  for (const source of imageSources) {
+    const img = new Image()
+    img.onload = () => {
+      loadingData.numOfLoaded += 1
+      if (loadingData.numOfLoaded == imageSources.length) {
+        loadingData.areImagesLoaded = true
+      }
+    }
+    img.src = source
+  }
 }
 
 @Component({
@@ -15,17 +24,52 @@ export function preloadImages() {
   styleUrl: './topics.component.css'
 })
 export class TopicsComponent {
-  imagesLoaded = true
+  constructor() {
+    preloadImages(this.topicData.map(item => item.coverImage), this.loadingData)
+  }
+
+  loadingData = {
+    areImagesLoaded: false,
+    numOfLoaded: 0
+  }
 
   topicData = [
-    { title: 'Sports & Exercise', coverImage: 'sport.svg', id: 2 },
-    { title: 'Video Games', coverImage: 'videogame.svg', id: 1 },
-    { title: 'Anime & Manga', coverImage: 'anime.svg', id: 5 },
-    { title: 'Food & Drink', coverImage: 'food.svg', id: 3 },
-    { title: 'Movies & TV Series', coverImage: 'movies.svg', id: 4 },
-    { title: 'Cars & Vehicles', coverImage: 'cars.svg', id: 8 },
-    { title: 'Books & Literature', coverImage: 'books.svg', id: 7 },
-    { title: 'Famous People', coverImage: 'celebrities.svg', id: 6 },
-    { title: 'Culture & History', coverImage: 'history.svg', id: 9 },
+    { 
+      title: 'Sports & Exercise', 
+      coverImage: 'https://pubhndccqdwypcouejkh.supabase.co/storage/v1/object/public/rankmaster/topic_images/sport.svg', 
+      id: 2 
+    },
+    { 
+      title: 'Video Games', 
+      coverImage: 'https://pubhndccqdwypcouejkh.supabase.co/storage/v1/object/public/rankmaster/topic_images/videogame.svg', 
+      id: 1 },
+    { 
+      title: 'Anime & Manga', 
+      coverImage: 'https://pubhndccqdwypcouejkh.supabase.co/storage/v1/object/public/rankmaster/topic_images/anime.svg', 
+      id: 5 },
+    { 
+      title: 'Food & Drink', 
+      coverImage: 'https://pubhndccqdwypcouejkh.supabase.co/storage/v1/object/public/rankmaster/topic_images/food.svg', 
+      id: 3 },
+    { 
+      title: 'Movies & TV Series', 
+      coverImage: 'https://pubhndccqdwypcouejkh.supabase.co/storage/v1/object/public/rankmaster/topic_images/movies.svg', 
+      id: 4 },
+    { 
+      title: 'Cars & Vehicles', 
+      coverImage: 'https://pubhndccqdwypcouejkh.supabase.co/storage/v1/object/public/rankmaster/topic_images/cars.svg', 
+      id: 8 },
+    { 
+      title: 'Books & Literature', 
+      coverImage: 'https://pubhndccqdwypcouejkh.supabase.co/storage/v1/object/public/rankmaster/topic_images/books.svg', 
+      id: 7 },
+    { 
+      title: 'Famous People', 
+      coverImage: 'https://pubhndccqdwypcouejkh.supabase.co/storage/v1/object/public/rankmaster/topic_images/celebrities.svg', 
+      id: 6 },
+    { 
+      title: 'Culture & History', 
+      coverImage: 'https://pubhndccqdwypcouejkh.supabase.co/storage/v1/object/public/rankmaster/topic_images/history.svg', 
+      id: 9 },
   ]
 }
